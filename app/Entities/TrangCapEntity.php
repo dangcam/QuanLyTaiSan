83399c187c1d2c3c1b379f62558d4b17aefd3ce4
Administrator <dangcam.pr@outlook.com>
2023-06-21 22:59:18 +0700

    Quyet dinh trang cap

---
+++ app/Entities/TrangCapEntity.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace App\Entities;
+use CodeIgniter\Entity\Entity;
+
+class DuAnEntity extends Entity{
+    protected $so_qd;
+    protected $ten_qd;
+    protected $don_vi;
+    protected $su_dung;
+}
